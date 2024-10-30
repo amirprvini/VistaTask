57504663060397675e5075f4f5ab51bc4b117404
amirparvini <amirparvini2000@gmail.com>
2024-10-30 13:31:41 +0330

    layout wrapped by container

--- vistatask/src/components/Layout/index.tsx
+++ vistatask/src/components/Layout/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Header from './SideBar'
 import { Outlet } from 'react-router-dom'
+import Container from '../Container'
 
 const LayOut: React.FC = (): JSX.Element => {
   return <div className='layOutWrapper flex h-full w-full'>
         <Header/>
-        <Outlet/>
+        <Container> <Outlet/> </Container>
     </div>
 }
 

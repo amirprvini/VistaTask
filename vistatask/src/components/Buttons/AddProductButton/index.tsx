12bbd4bdf952899aec3507d073b9a760594e4f06
amirparvini <amirparvini2000@gmail.com>
2024-11-03 11:54:39 +0330

    llast

--- vistatask/src/components/Buttons/AddProductButton/index.tsx
+++ vistatask/src/components/Buttons/AddProductButton/index.tsx
@@ -4,7 +4,7 @@ interface IAddProductButton {
     onClickProp: ()=> void 
 }
 const AddProductButton: React.FC<IAddProductButton> = ({onClickProp}): JSX.Element => {
-  return <button onClick={onClickProp} className={`addProduct flex font-poppins justify-center rounded-2xl items-center p-6 bg-gray-100 border border-gray-400 shadow-md w-80 h-full text-center font-bold text-2xl`}> + <br /> Add new <br/> product </button>
+  return <button onClick={onClickProp} className={`addProduct flex font-poppins justify-center rounded-2xl items-center p-6 bg-gray-100 border border-gray-400 shadow-md w-[17rem] h-full text-center font-bold text-2xl`}> + <br /> Add new <br/> product </button>
 }
 
 export default AddProductButton
